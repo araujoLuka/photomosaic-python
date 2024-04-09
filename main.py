7c51d382bdbceb072afee4f7412d3b053d580446
Lucas Araujo (Home) <lucasaraujo1@ufpr.br>
2024-04-09 09:25:21 -0300

    added function getAverageColor

--- main.py
+++ main.py
@@ -8,6 +8,20 @@ def openImage(path) -> Image.Image:
 def saveImage(image: Image.Image, path: str) -> None:
     image.save(path)
 
+def createMosaic(originalImage: Image.Image, datasetPath: str) -> Image.Image:
+    return Image.new("RGB", originalImage.size)
+
+def getAverageColor(image: Image.Image) -> tuple:
+    histogram = image.histogram()
+    r = histogram[0:256]
+    g = histogram[256:512]
+    b = histogram[512:768]
+    pixels = image.size[0] * image.size[1]
+    avgR = sum(i * r[i] for i in range(256)) // pixels
+    avgG = sum(i * g[i] for i in range(256)) // pixels
+    avgB = sum(i * b[i] for i in range(256)) // pixels
+    return (avgR, avgG, avgB)
+
 def main(originalImagePath: str = "image.jpg", datasetPath: str = "./data") -> int:
     print("Hello, World!")
     print("Photomosaic")
@@ -17,15 +31,18 @@ def main(originalImagePath: str = "image.jpg", datasetPath: str = "./data") -> i
     imageWidth, imageHeight = originalImage.size
     print(f"Image size: {imageWidth}x{imageHeight}")
 
+    orgAvgColor = getAverageColor(originalImage)
+
     print("Original Image Info:")
     print(" - Format: {0}".format(originalImage.format))
     print(" - Mode: {0}".format(originalImage.mode))
     print(" - Size: {0}".format(originalImage.size))
     print(" - Path: {0}".format(originalImagePath))
+    print(" - Average Color: {0}".format(orgAvgColor))
     originalImage.show()
 
     print("Creating new image...")
-    newImage = Image.new("RGB", (imageWidth, imageHeight))
+    newImage = Image.new("RGB", (imageWidth, imageHeight), color = orgAvgColor)
     saveImage(newImage, "output.jpg")
 
     print("New Image Info:")
@@ -33,8 +50,12 @@ def main(originalImagePath: str = "image.jpg", datasetPath: str = "./data") -> i
     print(" - Mode: {0}".format(newImage.mode))
     print(" - Size: {0}".format(newImage.size))
     print(" - Path: {0}".format("output.jpg"))
+    print(" - Average Color: {0}".format(getAverageColor(newImage)))
     print(" - Total images used: {0}".format(0))
     newImage.show()
+
+    print("Done.")
+    input("Press Enter to end...")
 
     return 0
 
